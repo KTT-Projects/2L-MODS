6ebaa4ee2e0d6cca3e13c10f9224f482d4d13da5
Tasuku24 <muscle.monkey2nd@gmail.com>
2024-06-21 02:25:24 +0900

    feat: Update client-server connection functionality and UI
    This commit improves the client-server connection functionality by adding support for both IPv4 and IPv6 addresses. It also updates the UI to display the connection status and the user's IP address. The "Connect" button triggers the connection process.

--- test.py
+++ test.py
@@ -1,25 +1,23 @@
 import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer
-tokenizer = AutoTokenizer.from_pretrained("stabilityai/stable-code-3b", trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained(
-  "stabilityai/stable-code-3b",
-  trust_remote_code=True,
-  # torch_dtype=torch.bfloat16,
-  torch_dtype="auto",
-)
-# model.cuda()
-# model.cuda(torch.device("mps"))
-# inputs = tokenizer("import torch\nimport torch.nn as nn", return_tensors="pt").to(model.device)
-# print(torch.backends.mps.is_available())
-inputs = tokenizer("""
-# Display positive integers from 1 to 100
-# Only prime numbers are allowed
-# Code is written in Python
-""", return_tensors="pt").to(model.device)
-tokens = model.generate(
-  **inputs,
-  max_new_tokens=48,
-  temperature=0.2,
-  do_sample=True,
-)
-print(tokenizer.decode(tokens[0], skip_special_tokens=True))
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+model_path = 'ahxt/LiteLlama-460M-1T'
+
+model = AutoModelForCausalLM.from_pretrained(model_path)
+model.to(torch.device('mps'))
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+model.eval()
+
+
+prompt = 'What is the future of the AI?'
+input = tokenizer(prompt, return_tensors="pt")
+token = model.generate(input, max_length=300)
+generated = tokenizer.decode(token[0].toList, skip_special_tokens=True)
+print(generated)
+# input = {key: value.to(torch.device('mps')) for key, value in input.items()}
+
+# with torch.no_grad():
+#     output = model.generate(**input, max_length=200, num_beams=5)
+
+# generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
+# print(generated_text)
