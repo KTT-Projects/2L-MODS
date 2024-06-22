4364a94267e270f6ebf72af552e4c3f8b4f0db7d
Takami Marsh <t-marsh@kttprojects.com>
2024-06-23 00:12:11 +0900

    Refactor client.py to handle connection status updates

--- client.py
+++ client.py
@@ -1,51 +1,72 @@
 import socket
+import threading
 
 HEADER = 64
 FORMAT = "utf-8"
 DISCONNECT_MESSAGE = "!DISCONNECT"
 
-clients = []
-connections = []
-message = ""
+# clients = []
+# connections = []
+# message = ""
 
 
-def connect_to_server(server, port, my_port):
-    global clients, connections
-    print(f"[CONNECTING TO SERVER] {server} : {port}")
-    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        client.connect((server, port))
-        clients.append(client)
-        if (server, port, "Failed") in connections:
-            connections[connections.index((server, port, "Failed"))] = (
-                server,
-                port,
-                "Connected",
-            )
-        else:
-            connections.append((server, port, "Connected"))
-        print(f"[CONNECTED TO SERVER] {server} : {port}")
-        print(f"[ACTIVE CONNECTIONS AS CLIENT] {len(clients)}")
-        send(str(my_port), client, len(clients) - 1)
-        return True
-    except:
-        connections.append((server, port, "Failed"))
-        print("[ERROR] Failed to connect to the server.")
-        return False
+# def connect_to_server(server, port, my_port):
+#     global clients, connections
+#     print(f"[CONNECTING TO SERVER] {server} : {port}")
+#     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#     try:
+#         client.connect((server, port))
+#         clients.append(client)
+#         if (server, port, "Failed") in connections:
+#             connections[connections.index((server, port, "Failed"))] = (
+#                 server,
+#                 port,
+#                 "Connected",
+#             )
+#         else:
+#             connections.append((server, port, "Connected"))
+#         print(f"[CONNECTED TO SERVER] {server} : {port}")
+#         print(f"[ACTIVE CONNECTIONS AS CLIENT] {len(clients)}")
+#         send(str(my_port), client, len(clients) - 1)
+#         return True
+#     except:
+#         connections.append((server, port, "Failed"))
+#         print("[ERROR] Failed to connect to the server.")
+#         return False
 
 
-def send(msg, client, index):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b" " * (HEADER - len(send_length))
-    client.send(send_length)
-    client.send(message)
-    print(f"[SENT] {msg}")
-    if msg == DISCONNECT_MESSAGE:
-        client.shutdown(socket.SHUT_RDWR)
-        client.close()
-        print("Disconnected from the server.")
-        clients.remove(client)
-        connections.remove((connections[index][0], connections[index][1], "Connected"))
-        return
+# def send(msg, client, index):
+#     message = msg.encode(FORMAT)
+#     msg_length = len(message)
+#     send_length = str(msg_length).encode(FORMAT)
+#     send_length += b" " * (HEADER - len(send_length))
+#     client.send(send_length)
+#     client.send(message)
+#     print(f"[SENT] {msg}")
+#     if msg == DISCONNECT_MESSAGE:
+#         client.shutdown(socket.SHUT_RDWR)
+#         client.close()
+#         print("Disconnected from the server.")
+#         clients.remove(client)
+#         connections.remove((connections[index][0], connections[index][1], "Connected"))
+#         return
+
+# def send(server_ip, server_port, client_socket, msg):
+
+
+def handle_connection(server_ip, server_port, client_socket):
+    print(f"[CONNECTED AS CLIENT] {server_ip} : {server_port}")
+    while True:
+        message = input("Enter message: ")
+        client_socket.sendto(message.encode(), (server_ip, server_port))
+        data, addr = client_socket.recvfrom(1024)
+        print(f"Received message from server: {data.decode()}")
+
+
+def connect_to_server(server_ip, server_port):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client_thread = threading.Thread(
+        target=handle_connection, args=(server_ip, server_port, client_socket, msg)
+    )
+    client_thread.start()
+    client_thread.join()
--- server.py
+++ server.py
@@ -1,61 +1,79 @@
 import socket
 import threading
-from requests import get
-from ipaddress import ip_address
 
 
 HEADER = 64
 FORMAT = "utf-8"
 DISCONNECT_MESSAGE = "!DISCONNECT"
-port = -1
-server_address = ""
-is_listening = False
-connections = dict()
+# port = -1
+# server_address = ""
+# is_listening = False
+# connections = dict()
 
 
-def handle_client(conn, addr):
-    global connections
-    connected = True
-    while connected:
-        try:
-            msg_length = conn.recv(HEADER).decode(FORMAT)
-            if not msg_length:
-                break
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
-            if connections.get(addr) is None:
-                connections[addr] = msg
-                print(f"[NEW CONNECTION] {addr[0]} : {msg} connected.")
-                print(f"[ACTIVE CONNECTIONS AS SERVER] {len(connections)}")
-            elif msg == DISCONNECT_MESSAGE:
-                connected = False
-            else:
-                print(f"[{addr[0]} : {connections[addr]}] {msg}")
-        except ConnectionResetError:
-            connected = False
-    conn.close()
-    connections.pop(addr)
-    print(f"[DISCONNECTED] {addr[0]} : {connections[addr]} disconnected.")
-    print(f"[ACTIVE CONNECTIONS AS SERVER] {len(connections)}")
+# def handle_client(conn, addr):
+#     global connections
+#     connected = True
+#     while connected:
+#         try:
+#             msg_length = conn.recv(HEADER).decode(FORMAT)
+#             if not msg_length:
+#                 break
+#             msg_length = int(msg_length)
+#             msg = conn.recv(msg_length).decode(FORMAT)
+#             if connections.get(addr) is None:
+#                 connections[addr] = msg
+#                 print(f"[NEW CONNECTION] {addr[0]} : {msg} connected.")
+#                 print(f"[ACTIVE CONNECTIONS AS SERVER] {len(connections)}")
+#             elif msg == DISCONNECT_MESSAGE:
+#                 connected = False
+#             else:
+#                 print(f"[{addr[0]} : {connections[addr]}] {msg}")
+#         except ConnectionResetError:
+#             connected = False
+#     conn.close()
+#     connections.pop(addr)
+#     print(f"[DISCONNECTED] {addr[0]} : {connections[addr]} disconnected.")
+#     print(f"[ACTIVE CONNECTIONS AS SERVER] {len(connections)}")
 
 
-def start_server(external_ip, external_port):
-    global port
-    global server_address
-    global is_listening
-    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    addr = ("", external_port)
-    try:
-        server.bind(addr)
-    except OSError:
-        print(f"[ERROR] Port {external_port} is already in use.")
-        return
-    port = external_port
-    server_address = external_ip
-    server.listen()
-    is_listening = True
-    print(f"Listening on {server_address} : {port}")
+# def start_server(external_ip, external_port):
+#     global port
+#     global server_address
+#     global is_listening
+#     server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#     addr = ("", external_port)
+#     try:
+#         server.bind(addr)
+#     except OSError:
+#         print(f"[ERROR] Port {external_port} is already in use.")
+#         return
+#     port = external_port
+#     server_address = external_ip
+#     server.listen()
+#     is_listening = True
+#     print(f"Listening on {server_address} : {port}")
+#     while True:
+#         conn, addr = server.accept()
+#         thread = threading.Thread(target=handle_client, args=(conn, addr))
+#         thread.start()
+
+
+def handle_client(server_socket):
+    print("[SERVER STARTED] Waiting for connections...")
     while True:
-        conn, addr = server.accept()
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
-        thread.start()
+        msg_length, addr = server_socket.recvfrom(HEADER)
+        msg_length = int(msg_length.decode(FORMAT))
+        data, addr = server_socket.recvfrom(msg_length)
+        print(f"Received message from {addr}: {data.decode(FORMAT)}")
+        if data:
+            message = "This is an automated response."
+            server_socket.sendto(message.encode(), addr)
+
+
+def start_server(external_port):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket.bind(("", external_port))
+    server_thread = threading.Thread(target=handle_client, args=(server_socket,))
+    server_thread.start()
+    server_thread.join()
