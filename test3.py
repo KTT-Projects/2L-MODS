4d5ed2a2da338064d504ebef3692850b9c06b3f2
Tasuku24 <muscle.monkey2nd@gmail.com>
2024-06-23 12:50:24 +0900

    Update neural network architecture in test3.py

--- test3.py
+++ test3.py
@@ -5,20 +5,25 @@ from collections import OrderedDict
 
 torch.manual_seed(0)
 
-net_seq = nn.Sequential(OrderedDict([
-  ('fc1', nn.Linear(4, 4)),
-  ('relu', nn.ReLU()),
-  ('dropout', nn.Dropout(0.5)),
-  ('fc2', nn.Linear(4, 2))
-]))
+# class NetFunctional(nn.Module):
+#     def __init__(self, input, hidden, output, dropout):
+#         super().__init__()
+#         self.fc1 = nn.Linear(input, hidden)
+#         self.fc2 = nn.Linear(hidden, output)
+#         self.relu = nn.ReLU()
+#         self.dropout = nn.Dropout(dropout)
 
-print('net_seq:',net_seq)
-print('type:', type(net_seq))
-print('issubclass:', issubclass(type(net_seq), nn.Module))
-print('1st:', net_seq[0])
-print('weight:', net_seq[0].weight)
-print('bias:', net_seq[0].bias)
-print('in:', net_seq[0].in_features)
-print('out:', net_seq[0].out_features)
-print('weight_data:', net_seq[0].weight.data)
-print('tensor:', torch.tensor([1, 2, 3, 4], dtype=torch.float32))
+#     def forward(self, x):
+#         x = self.fc1(x)
+#         x = self.relu(x)
+#         x = self.dropout(x)
+#         x = self.fc2(x)
+#         return x
+
+model = nn.Sequential(
+  nn.Linear(10, 5),
+  nn.Linear(5, 2)
+)
+
+def forward_hook(model, input, output):
+  print('input:', input[0].shape, 'output:', output.shape)
