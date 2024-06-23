1fddb7dbebf7b76a2747175d898f0741d3e34a6b
Takami Marsh <t-marsh@kttprojects.com>
2024-06-23 13:17:35 +0900

    Refactor client.py to update server connection logic

--- server.py
+++ server.py
@@ -6,35 +6,6 @@ from config import *
 clients = []
 
 
-# def maintain_connection(server_socket, addr):
-#     while True:
-#         try:
-#             data = IGNORE_MESSAGE
-#             server_socket.sendto(data.encode(), addr)
-#             time.sleep(0.5)
-#         except ConnectionResetError:
-#             print(f"[CONNECTION LOST] Connection lost with {addr}")
-#             break
-
-
-# def handle_client(server_socket, addr):
-#     print(f"[NEW CONNECTION] Connected to {addr}")
-#     connection_thread = threading.Thread(
-#         target=maintain_connection, args=(server_socket, addr)
-#     )
-#     connection_thread.start()
-#     while True:
-#         data = server_socket.recv(SIZE)
-#         if data == DISCONNECT_MESSAGE:
-#             print(f"[CONNECTION CLOSED] Disconnected from {addr}")
-#             break
-#         elif data == IGNORE_MESSAGE:
-#             continue
-#         print(f"[{addr}] {data.decode()}")
-#     server_socket.close()
-#     print(f"[CONNECTION CLOSED] Disconnected from {addr}")
-
-
 def send_to_client(server_socket, addr, msg):
     server_socket.sendto(msg.encode(), addr)
 
